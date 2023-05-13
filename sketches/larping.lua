813cea68d10882b3a6f02f26f5c8b125874ab81f
Scott Stahl <stahl@hey.com>
2023-05-12 21:09:11 -0400

    new arper

--- sketches/larping.lua
+++ sketches/larping.lua
@@ -1,22 +1,30 @@
---- arp quantizer & progressionator
--- scott stahl 2023
--- in1: clock pulse which will advance arp selection and randomize some of the envelope values
--- in2: voltage to quantize
--- out1: in2 quantized to current arp continuously
--- out2: triangle envelope on note change
--- out3: envelope fired on note change
--- out4: reverse envelope fire on note change
+--- fun dual arpeggiator
+-- in1 voltage to quantize
+-- in2 window to change scale
+-- out1 voltage quantized to scale
+-- out2 ar envelope when note changes
+-- out3 free running sequence matching selected scale
+-- out4 ar envelope when note changes
 
 s = sequins
 
--- adjust these variables to taste
-seq = s{
-  'majTri'
-, 'maj7th'
-, 'dom7th'
-, 'min7th'
-, 'dim7th'
-}
+maj = s{0,2,4,s{7,9}}
+majr = s{9,7,4,2,0}
+min = s{0,3,5,s{7,11}}
+minr = s{11,7,5,3,0}
+
+majQ = {0,2,4,7,9}
+minQ = {0,3,5,7,11}
+
+arps = {minr, min, maj, majr} 
+qs = {minQ, minQ, majQ, majQ}
+
+arp = arps[3]
+q = qs[3]
+
+-- customize while playing
+o = 12 --offset
+rate = 1 -- lower number = faster, can do fractions
 
 env_length1 = 0.8 -- length of first ar envelope
 ar_level1 = 2 -- voltage of first ar envelope
@@ -24,48 +32,52 @@ ar_level1 = 2 -- voltage of first ar envelope
 env_length2 = 0.8 -- length of second ar envelope
 ar_level2 = 2 -- voltage of second ar envelope
 
-arp_progression = {
-  majTri = {0,4,7}
-, maj7th = {0,4,7,11}
-, dom7th = {0,4,7,10}
-, min7th = {0,3,7,10}
-, dim7th = {0,3,6,9}
-}
-
-function requantize()
-  local next = seq()
-  print('new arp:', next)
-  input[2].mode('scale', arp_progression[next])
+function maybe_offset(n)
+  if (math.random() > 0.8) then
+    return n+o
+  elseif (math.random() > 0.6) then
+    return n-o
+  else
+    return n
+  end
 end
 
-function randomize_length_and_v()
-  env_length1 = math.random(1, 10) / 10
-  ar_level1 = math.random(1, 3)
-  print('ar1 length, level:', env_length1, ar_level1)
-  env_length2 = math.random(1, 10) / 10
-  ar_level2 = math.random(1, 3)
-  print('ar2 length, level:', env_length2, ar_level2)
+function new_note()
+  return maybe_offset(arp()) / 12
 end
 
--- change scale on 1v increase
-input[1].change = function(state)
-  requantize()
-  randomize_length_and_v()
+function play_note(v)
+  output[3].volts = v
+end
+
+function run_arp()
+  return clock.run(
+    function()
+      while true do
+        clock.sync(rate)
+        local note = new_note()
+        play_note( note )
+        output[4]()
+      end
+    end
+  )
 end
 
 -- update continuous quantizer
-input[2].scale = function(s)
+input[1].scale = function(s)
   output[1].volts = s.volts
-  -- fire all output modulation events on note change
   output[2]()
-  output[3]()
-  output[4]()
+end
+
+input[2].window = function(win, dir)
+  arp = arps[win]
+  q = qs[win]
 end
 
 function init()
-  input[1].mode('change',1,0.1,'rising')
-  input[2].mode('scale', arp_progression[seq[1]])
-  output[2].action = ar((env_length1 / 2), (env_length2 / 2), (ar_level1 + ar_level2) / 2) -- even triangle
-  output[3].action = ar(0.01, env_length1, ar_level1) -- quick attack long release on note change
-  output[4].action = ar(env_length2, 0.01, ar_level2) -- long attack quick release on note change
+  input[1].mode('scale', q)
+  input[2].mode('window', {-2,-0.1,2})
+  output[2].action = ar(0.01, env_length1, ar_level1)
+  myarp = run_arp(arp)
+  output[4].action = ar(0.01, env_length2, ar_level2)
 end
